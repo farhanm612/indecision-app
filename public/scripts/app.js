a5c486458a76d5d80c4f581d506f1f53c21f724d
Farhan Mahmood <farhanm612@gmail.com>
2018-08-15 18:56:15 +0500

    Conditional Rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,11 +2,66 @@
 
 console.log('App.js is running!');
 
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put life in the hands of a computer'
+};
+
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item 1'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item 2'
+        )
+    )
+);
+
+var user = {
+    name: 'Farhan',
+    age: 23,
+    location: 'Lahore'
+};
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.location
+    )
 );
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,34 @@
 console.log('App.js is running!');
 
-var template = <h1>Indecision App</h1>
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put life in the hands of a computer'
+};
+
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
+        <ol>
+            <li>Item 1</li>
+            <li>Item 2</li>
+        </ol>
+    </div>
+);
+
+
+var user = {
+    name: 'Farhan',
+    age: 23,
+    location: 'Lahore'
+};
+var templateTwo = (
+    <div>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+);
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template,appRoot);
+ReactDOM.render(templateTwo,appRoot);
